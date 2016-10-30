7ed24625c044badef74c0c12b0fa23c2abf15867
Diego Feitosa <dnfeitosa@gmail.com>
2016-10-29 19:23:03 -0700

    removing server, core & db projects and leaving only the index support so this becomes a new repository containing only indexing functionality without losing history on what has been developed

--- codegraph-core/src/main/java/com/dnfeitosa/codegraph/core/utils/PathUtils.java
+++
@@ -1,9 +0,0 @@
-package com.dnfeitosa.codegraph.core.utils;
-
-import org.apache.commons.lang.StringUtils;
-
-public class PathUtils {
-    public static String join(Object... paths) {
-        return StringUtils.join(paths, "/");
-    }
-}
